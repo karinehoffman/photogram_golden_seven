0a5e5f38b0d91f2cd78f670380dc07a8c1152779
karinehoffman <khoffman2016@kellogg.northwestern.edu>
2015-05-10 16:07:12 -0700

    Updated all

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,40 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params["id"]})
+  end
+
+  def new_form
+  end
+
+  def create_row
+    i = Photo.new
+    i.source = params[:the_source]
+    i.caption = params[:the_caption]
+    i.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find(params[:id])
+  end
+
+  def update_row
+    i=Photo.find(params[:id])
+    i.source = params[:the_source]
+    i.caption = params[:the_caption]
+    i.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    i = Photo.find(params[:id])
+    i.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
 
 end
